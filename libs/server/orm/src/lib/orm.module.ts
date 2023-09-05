568ac99da663fc3dabc62712156f1f40f5939421
Ivan Konnikov <ivan.liaskin@gmail.com>
2023-09-05 21:03:18 +0200

    develop: Added functionality to ORM service.

--- libs/server/orm/src/lib/orm.module.ts
+++ libs/server/orm/src/lib/orm.module.ts
@@ -2,11 +2,12 @@ import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { Module } from '@nestjs/common';
 import { OrmService } from './orm.service';
 import { ToDo } from './entities/ToDo.entity';
+import { User } from './entities/User.entity';
 
 @Module({
   imports: [
     MikroOrmModule.forRoot({
-      entities: [ToDo],
+      entities: [ToDo, User],
       dbName: 'to-dos`',
       type: 'postgresql',
     }),
--- libs/server/orm/src/lib/orm.service.ts
+++ libs/server/orm/src/lib/orm.service.ts
@@ -1,5 +1,6 @@
 import { EntityManager, MikroORM } from '@mikro-orm/core';
 import { Injectable } from '@nestjs/common';
+import { ToDo } from './entities/ToDo.entity';
 
 @Injectable()
 export class OrmService {
@@ -7,4 +8,22 @@ export class OrmService {
     private readonly orm: MikroORM,
     private readonly em: EntityManager
   ) {}
+
+  async add(todo: ToDo) {
+    await this.em.persistAndFlush(todo);
+  }
+
+  async getAll() {
+    return this.em.find(ToDo, {});
+  }
+
+  async findRecordById(id: string) {
+    return this.em.find(ToDo, { id });
+  }
+
+  async removeRecordById(id: string) {
+    const foundRecord = this.findRecordById(id);
+
+    if (foundRecord) await this.em.removeAndFlush(foundRecord);
+  }
 }
